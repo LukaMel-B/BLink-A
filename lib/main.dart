7f8f97172a67b96884e8c9bab94900fda99cbaf5
LukaMel-B <luluhsany@sscollege.ac.in>
2022-02-18 20:50:12 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -15,10 +15,12 @@ import 'Contents//parent-profile-edit.dart';
 import 'Contents//parent-profile-view.dart';
 import 'Contents//Setting.dart';
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
